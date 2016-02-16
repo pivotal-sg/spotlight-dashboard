c873e67c324f6bb03e74286ba31a3482e31038fb
Divya Bhargov <divya.bhargov@neo.com>
2016-02-16 16:58:04 +0800

    Fix fetch on non-chrome browsers

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const plugins = [
     new Webpack.ProvidePlugin({
-      'Promise': 'es6-promise',
       'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
     }),
     new ExtractTextPlugin("style.css", {
