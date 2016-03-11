9fe26ff92b8dd9a4a9075567eead6efeafff2fc5
Divya Bhargov divya.bhargov@neo.com <pair+db@neo.com>
2016-03-11 14:32:57 +0800

    Ding at 9.06

--- app/components/clock_widget/clock-widget.js
+++ app/components/clock_widget/clock-widget.js
@@ -25,16 +25,25 @@ const ClockWidget = React.createClass({
     clearInterval(this.state.timerId);
   },
 
+  ding: function() {
+    const sound = document.getElementById('audio');
+    sound.play();
+  },
+
   updateClock: function() {
     this.setState({
       time: moment().format('H:mm:ss'),
       date: moment().format('dddd, DD MMM')
     });
+    if (moment().format('HH:mm:ss') === '09:06:00') {
+      this.ding();
+    }
   },
 
   render: function() {
     return (
       <div className="clock">
+        <audio id="audio" src="http://www.soundjay.com/mechanical/sounds/clong-1.mp3" autostart="false" />
         <div className="time"> {this.state.time} </div>
         <div className="date"> {this.state.date} </div>
       </div>
