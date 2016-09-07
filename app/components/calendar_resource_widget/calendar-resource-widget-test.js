8d24e97810a0b673aafb00bfdf45bf13401e13e0
Pivotal Pair <pair@pivotal.io>
2016-09-07 11:14:15 +0800

    Add CalendarResourceWidget
    Used to render the current state of the room.

---
+++ app/components/calendar_resource_widget/calendar-resource-widget-test.js
@@ -0,0 +1,85 @@
+const React = require('react');
+const TestUtils = require('react/lib/ReactTestUtils');
+
+const chai = require('chai');
+const expect = require('chai').expect;
+const sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+
+const CalendarResourceWidget = require('./calendar-resource-widget');
+
+describe('CalendarResourceWidget', function() {
+  let fakeFetch;
+  let component;
+
+  const widgetProps = {
+    title: 'PL Calendar',
+    uuid: 'some_uuid',
+  };
+
+  describe('when it is available', function() {
+    beforeEach( function() {
+      const availableResponse = {
+        available: true,
+        next_available_at: null,
+        next_booking_at: '2016-09-07T06:00:00+00:00'
+      };
+      const res = new window.Response(JSON.stringify(availableResponse), {
+        status: 200,
+        headers: {
+          'Content-type': 'application/json'
+        }
+      });
+      fakeFetch = sinon.stub(window, 'fetch');
+      window.fetch.returns(Promise.resolve(res));
+
+      component = TestUtils.renderIntoDocument( <CalendarResourceWidget { ...widgetProps }/>);
+    });
+
+    afterEach(function() {
+      window.fetch.restore();
+    });
+
+    it("shows 'Available'", function(done) {
+      setTimeout(function(){
+        const body = component.getDOMNode().innerHTML;
+        expect(body).to.include('Available');
+        done();
+      });
+    });
+  });
+
+  describe('when it is booked', function() {
+    beforeEach( function() {
+      const bookedResponse = {
+        available: false,
+        next_available_at: null,
+        next_booking_at: '2016-09-07T06:00:00+00:00'
+      };
+      const res = new window.Response(JSON.stringify(bookedResponse), {
+        status: 200,
+        headers: {
+          'Content-type': 'application/json'
+        }
+      });
+      fakeFetch = sinon.stub(window, 'fetch');
+      window.fetch.returns(Promise.resolve(res));
+
+      component = TestUtils.renderIntoDocument( <CalendarResourceWidget { ...widgetProps }/>);
+    });
+
+    afterEach(function() {
+      window.fetch.restore();
+    });
+
+    it("shows 'Booked'", function(done) {
+      setTimeout(function(){
+        const body = component.getDOMNode().innerHTML;
+        expect(body).to.include('Booked');
+        done();
+      });
+    });
+  });
+});
+
--- server.js
+++ server.js
@@ -3,17 +3,23 @@ var path = require('path');
 var isDevelopment = (process.env.NODE_ENV !== 'production');
 var static_path = path.join(__dirname, 'public');
 
+process.on( 'SIGINT', function() {
+    console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
+    process.exit( );
+})
+
 if (isDevelopment) {
   var config = require('./webpack.config');
   var WebpackDevServer = require('webpack-dev-server');
   var webpack = require('webpack');
+  var port = process.env.SPOTLIGHT_DASHBOARD_PORT ? process.env.SPOTLIGHT_DASHBOARD_PORT : 9000;
 
   new WebpackDevServer(webpack(config), {
     contentBase: static_path,
     hot: true
-  }).listen(9000, '0.0.0.0', function (err, result) {
+  }).listen(port, '0.0.0.0', function (err, result) {
     if (err) { console.log(err) }
-    console.log('Listening at localhost:9000 in DEV');
+    console.log('Listening at localhost:' + port + ' in DEV');
   });
 } else {
   var express = require('express');
